08a5992159a180b71d62a3129280f36cf4fac5ea
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-03-29 16:10:39 +0200

    deep copy

---
+++ src/DemoForm/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {style} from './index.scss';
+
+const DemoForm = ({}) => {
+
+    return (
+        <div className={style}>
+            
+        </div>
+    );
+};
+
+DemoForm.propTypes = {
+
+};
+
+export default DemoForm;
